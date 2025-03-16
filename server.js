82885e350f9087a83f757751853c2b3ea42c86ea
Aleem Talha <aleemtalha098@gmail.com>
2025-03-16 22:15:55 +0500

    vercel updates

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const xssClean = require("xss-clean");
-const app = express();
 const compression = require("compression");
 const csrf = require("csurf");
 const console = require("debug")("development:app");
@@ -10,43 +9,49 @@ const config = require("config");
 require("dotenv").config();
 const mongoSanitize = require("express-mongo-sanitize");
 require("./config/mongoose-connection");
-
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const morgan = require("morgan");
+const serverless = require("serverless-http");
+
 const { isAdmin } = require("./middlewares/isAdmin");
 const { isLoggedIn } = require("./middlewares/isLoggedIn");
-const { adminLimiter } = require("./utils/rateLimiter");
-const { superAdminLimiter } = require("./utils/rateLimiter");
+const { adminLimiter, superAdminLimiter } = require("./utils/rateLimiter");
+
+const app = express();
 
 app.use(express.json());
 app.use(helmet());
 app.use(csrf());
 app.use(mongoSanitize());
-app.use(xssClean()); 
+app.use(xssClean());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(express.static("public"));
+
 let Mongo_URI = config.get("MONGO_URI");
 Mongo_URI = Mongo_URI.replace("<db_password>", process.env.DB_PASSWORD);
 Mongo_URI = Mongo_URI.replace("<dbname>", process.env.DB_NAME);
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false, 
+    saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: Mongo_URI }),
-    cookie: { 
+    cookie: {
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
-      sameSite: "strict" 
+      sameSite: "strict",
     },
   })
 );
+
 app.disable("x-powered-by");
 app.use(compression());
 app.use(cookieParser());
+
 const FRONTEND_URL = config.get("FRONT_END_URI");
 app.use(
   cors({
@@ -63,23 +68,25 @@ app.use((req, res, next) => {
   next();
 });
 
+
+const router = express.Router();
+
+router.get("/hello", (req, res) => {
+  res.json({ message: "Hello from Vercel!" });
+});
+
+app.use("/api", router);
 app.use("/super/admin", superAdminLimiter, require("./routes/superAdmin"));
 app.use("/admin", isLoggedIn, isAdmin, adminLimiter, require("./routes/admin"));
 app.use("/", require("./routes/user"));
 
-app.use((req, res, next) => {
-  res
-    .status(404)
-    .json({
-      success: false,
-      message:
-        "Error 404!\n Not found.\n\nSorry the page you are looking for has been deleted or does not exists.",
-    });
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message:
+      "Error 404!\n Not found.\n\nSorry, the page you are looking for has been deleted or does not exist.",
+  });
 });
 
-const host = process.env.HOST || "localhost";
-const port = process.env.PORT || 3001;
-
-app.listen(port, host, () => {
-  console(`Server running at: http://${host}:${port}`);
-});
+module.exports = app;
+module.exports.handler = serverless(app);
