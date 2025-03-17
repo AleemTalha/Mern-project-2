84f7ca2b2826fd18fd7a967297ceef38d809eef4
Aleem Talha <aleemtalha098@gmail.com>
2025-03-17 16:59:18 +0500

    msgs

--- server.js
+++ server.js
@@ -2,82 +2,77 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const xssClean = require("xss-clean");
-const app = express();
 const compression = require("compression");
-const csrf = require("csurf");
-const debug = require("debug")("development:app");
-const config = require("config");
-require("dotenv").config();
-const mongoSanitize = require("express-mongo-sanitize");
-require("./config/mongoose-connection");
-
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const morgan = require("morgan");
+const mongoSanitize = require("express-mongo-sanitize");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const { isAdmin } = require("./middlewares/isAdmin");
 const { isLoggedIn } = require("./middlewares/isLoggedIn");
-const { adminLimiter } = require("./utils/rateLimiter");
-const { superAdminLimiter } = require("./utils/rateLimiter");
+const { adminLimiter, superAdminLimiter } = require("./utils/rateLimiter");
 
+dotenv.config();
+
+const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(mongoSanitize());
-app.use(xssClean()); 
-app.use(express.urlencoded({ extended: true }));
+app.use(xssClean());
 app.use(morgan("dev"));
+app.use(compression());
+app.use(cookieParser());
 app.use(express.static("public"));
-let Mongo_URI = config.get("MONGO_URI");
-Mongo_URI = Mongo_URI.replace("<db_password>", process.env.DB_PASSWORD);
-Mongo_URI = Mongo_URI.replace("<dbname>", process.env.DB_NAME);
+
+const MONGO_URI = process.env.MONGO_URI.replace("<db_password>", process.env.DB_PASSWORD).replace("<dbname>", process.env.DB_NAME);
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB Connection Error:", err));
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false, 
-    store: MongoStore.create({ mongoUrl: Mongo_URI }),
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: MONGO_URI }),
     cookie: {
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
-      sameSite: "strict" 
+      sameSite: "strict"
     },
   })
 );
-app.disable("x-powered-by");
-app.use(compression());
-app.use(cookieParser());
 
-// const FRONTEND_URL = config.get("FRONT_END_URI");
-// app.use(
-//   cors({
-//     origin: FRONTEND_URL,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
-// app.use((req, res, next) => {
-//   if (req.headers.origin !== FRONTEND_URL) {
-//     return res.status(403).json({ success: false, message: "Access Denied!" });
-//   }
-//   next();
-// });
+const FRONTEND_URL = process.env.FRONTEND_URL || "*";
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use("/super/admin", superAdminLimiter, require("./routes/superAdmin"));
 app.use("/admin", isLoggedIn, isAdmin, adminLimiter, require("./routes/admin"));
 app.use("/", require("./routes/user"));
 
-app.use((req, res, next) => {
-  res
-    .status(404)
-    .json({
-      success: false,
-      message:
-        "Error 404!\n Not found.\n\nSorry the page you are looking for has been deleted or does not exists.",
-    });
-})
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Error 404! Not found. Sorry, the page you are looking for has been deleted or does not exist."
+  });
+});
 
 const port = process.env.PORT || 3001;
+const server = app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
 
-app.listen(port, () => {
-  console.log(`Server running at: port ${port}`);
+process.on("SIGTERM", () => {
+  console.log("Server shutting down...");
+  server.close(() => process.exit(0));
 });
