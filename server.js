338cc074e031e3df0c4a3edd89e141a3caf62fca
Aleem Talha <aleemtalha098@gmail.com>
2025-03-17 23:22:08 +0500

    final touches

--- server.js
+++ server.js
@@ -10,22 +10,31 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
+const xssClean = require("xss-clean");
+const csrf = require("csurf");
+const { isAdmin } = require("./middlewares/isAdmin");
+const { isLoggedIn } = require("./middlewares/isLoggedIn");
+const { adminLimiter, superAdminLimiter } = require("./utils/rateLimiter");
 
 const app = express();
 
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(helmet());
+app.use(csrf());
 app.use(mongoSanitize());
+app.use(xssClean());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(compression());
 app.use(cookieParser());
 
 let MONGO_URI = config.get("MONGO_URI");
-
 if (MONGO_URI.includes("<db_password>") && process.env.DB_PASSWORD) {
   MONGO_URI = MONGO_URI.replace("<db_password>", process.env.DB_PASSWORD);
+}
+if (MONGO_URI.includes("<dbname>") && process.env.DB_NAME) {
+  MONGO_URI = MONGO_URI.replace("<dbname>", process.env.DB_NAME);
 }
 
 app.use(
@@ -52,7 +61,7 @@ app.use(
       if (!origin || FRONTEND_URLS.includes(origin)) {
         callback(null, true);
       } else {
-        callback(null, false); 
+        callback(null, false); // No error thrown, just rejecting the origin
       }
     },
     methods: ["GET", "POST", "PUT", "DELETE"],
@@ -61,17 +70,19 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  if (!req.headers.origin || FRONTEND_URLS.includes(req.headers.origin)) {
+  const origin = req.headers.origin;
+  
+  if (!origin || FRONTEND_URLS.includes(origin)) {
     return next();
   }
+
   return res.status(403).json({
     success: false,
     message: "🚫 Access Denied! You are not authorized to view this content. 🔒",
-    suggestion: "🔄 Please check your permissions or contact support if you believe this is an error."
+    suggestion:
+      "🔄 Please check your permissions or contact support if you believe this is an error.",
   });
 });
-
-
 
 
 app.get("/", (req, res) => {
@@ -81,6 +92,8 @@ app.get("/", (req, res) => {
   });
 });
 
+app.use("/super/admin", superAdminLimiter, require("./routes/superAdmin"));
+app.use("/admin", isLoggedIn, isAdmin, adminLimiter, require("./routes/admin"));
 app.use("/", require("./routes/user"));
 
 app.use((req, res) => {
@@ -91,7 +104,9 @@ app.use((req, res) => {
   });
 });
 
+const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 3001;
+
 const server = app.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
@@ -102,3 +117,5 @@ process.on("SIGTERM", () => {
     console.log("Process terminated.");
   });
 });
+
+module.exports = app;
