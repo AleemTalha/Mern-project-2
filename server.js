bb302f45e469bb5b8da59c29aa893202ca871851
Aleem Talha <aleemtalha098@gmail.com>
2025-03-19 03:59:15 +0500

    otp verify

--- server.js
+++ server.js
@@ -71,6 +71,7 @@ app.use(
         callback(new Error("🚫 Not allowed by CORS"));
       }
     },
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
