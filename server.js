40b42272524709d6302cfdf40993afec6dd31fc4
Aleem Talha <aleemtalha098@gmail.com>
2025-03-18 21:54:43 +0500

    frontend testers

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const { adminLimiter, superAdminLimiter } = require("./utils/rateLimiter");
 
 const app = express();
 
+app.set("trust proxy", 1);
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(helmet());
