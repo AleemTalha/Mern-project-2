7670d369b3d324ca9e5f5c0b41294d5d7decd8ff
Aleem Talha <aleemtalha098@gmail.com>
2025-03-18 22:27:26 +0500

    front tester

--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -24,7 +24,7 @@ const adminLimiter = rateLimit({
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 50,
+  max: 5,
   handler: (req, res) => {
     res.status(429).json({
       success: false,
