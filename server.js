ee8be7c9be76f6742b4e32b06de24352f66d498e
Aleem Talha <aleemtalha098@gmail.com>
2025-03-20 15:58:15 +0500

    session issues

--- server.js
+++ server.js
@@ -58,6 +58,12 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  console.log("Session ID:", req.sessionID);
+  console.log("Cookies:", req.cookies);
+  console.log("Session Data:", req.session);
+  next();
+});
 
 app.disable("x-powered-by");
 
