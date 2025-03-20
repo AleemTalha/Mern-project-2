0f490a0a16771b4b771c2cc6116cf091e6003efb
Aleem Talha <aleemtalha098@gmail.com>
2025-03-20 16:45:31 +0500

    mesg

--- server.js
+++ server.js
@@ -64,13 +64,19 @@ app.disable("x-powered-by");
 const FRONTEND_URLS = config.get("FRONT_END_URI");
 const allowedOrigins = Array.isArray(FRONTEND_URLS) ? FRONTEND_URLS : [FRONTEND_URLS];
 
-app.use((req, res, next) => { 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie");
+  res.header("Access-Control-Allow-Credentials", "true");
+
   if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     return res.sendStatus(204);
   }
+
   next();
 });
+
 
 app.use(
   cors({
