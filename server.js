fe412030797fe345db23fd97d4b79623630614f7
Aleem Talha <aleemtalha098@gmail.com>
2025-03-20 16:08:23 +0500

    session issues

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  console.log("Cookies received:", req.cookies);
+  console.log("🔍 Cookies Received:", req.headers.cookie || "❌ No Cookies Received");
   next();
 });
 
