7674f54989d07851c5d44bdaf9b74d3d93f331e7
Aleem Talha <aleemtalha098@gmail.com>
2025-03-20 16:51:12 +0500

    mg

--- server.js
+++ server.js
@@ -64,11 +64,15 @@ app.disable("x-powered-by");
 const FRONTEND_URLS = config.get("FRONT_END_URI");
 const allowedOrigins = Array.isArray(FRONTEND_URLS) ? FRONTEND_URLS : [FRONTEND_URLS];
 
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin || "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie");
-  res.header("Access-Control-Allow-Credentials", "true");
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie");
+    res.header("Access-Control-Allow-Credentials", "true");
+  }
 
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
@@ -76,22 +80,6 @@ app.use((req, res, next) => {
 
   next();
 });
-
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("🚫 Not allowed by CORS"));
-      }
-    },
-    credentials: true, 
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],  
-    allowedHeaders: ["Content-Type", "Authorization", "Cookie"], 
-  })
-);
 
 
 app.use((req, res, next) => {
