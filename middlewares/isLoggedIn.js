6ae982e75ebb68a91e275db5c845c67ecaa50d64
Aleem Talha <aleemtalha098@gmail.com>
2025-04-04 22:02:28 +0500

    msg

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
-const console = require("debug")("development:auth");
+const debug= require("debug")("development:auth");
 
 const isLoggedIn = async (req, res, next) => {
   if (req.session.user && req.session.token && req.cookies.token) {
@@ -9,7 +9,7 @@ const isLoggedIn = async (req, res, next) => {
       req.user = req.session.user;
       return next();
     } catch (err) {
-      console("Error: Invalid session token");
+      console.log("Error: Invalid session token");
       return res.status(401).json({
         loggedIn: false,
         success: false,
@@ -24,11 +24,11 @@ const isLoggedIn = async (req, res, next) => {
   if (!req.cookies.token) missing.push("Cookie Token");
 
   if (missing.length > 0) {
-    console(`Missing: ${missing.join(", ")}`);
+    console.log(`Missing: ${missing.join(", ")}`);
   }
 
   if (!req.cookies.token) {
-    console("Error: No cookie token provided");
+    console.log("Error: No cookie token provided");
     return res.status(401).json({
       success: false,
       message: "Unauthorized access, No cookie token provided",
@@ -40,14 +40,14 @@ const isLoggedIn = async (req, res, next) => {
       req.cookies.token,
       process.env.ACCESS_TOKEN_SECRET
     );
-    console(`Running DB Query: Finding user with ID ${decoded.id}`);
+    console.log(`Running DB Query: Finding user with ID ${decoded.id}`);
 
     let user = await userModel
       .findOne({ _id: decoded.id })
       .select("email fullname status role location profileImage");
 
     if (!user) {
-      console("Error: Invalid email, user not found in DB");
+      console.log("Error: Invalid email, user not found in DB");
       return res.status(401).json({
         success: false,
         message: "Unauthorized access, Invalid email",
@@ -55,7 +55,7 @@ const isLoggedIn = async (req, res, next) => {
       });
     }
     if (user.status === "inactive") {
-      console("Error: User is inactivated, destroying all cookies");
+      console.log("Error: User is inactivated, destroying all cookies");
       Object.keys(req.cookies).forEach((cookie) => res.clearCookie(cookie));
       return res.status(401).json({
         success: false,
@@ -97,14 +97,14 @@ const isLoggedIn = async (req, res, next) => {
       });
       req.session.token = accessToken;
       req.session.cookie.maxAge = maxAge;
-      console("Session initialized from cookie token");
+      console.log("Session initialized from cookie token");
     }
     if (
       !req.session.user ||
       !req.session.token ||
       req.session.token !== req.cookies.token
     ) {
-      console("Error: Session mismatch detected, destroying all cookies");
+      console.log("Error: Session mismatch detected, destroying all cookies");
       Object.keys(req.cookies).forEach((cookie) => res.clearCookie(cookie));
       return res.status(401).json({
         success: false,
@@ -114,7 +114,7 @@ const isLoggedIn = async (req, res, next) => {
     req.user = req.session.user;
     next();
   } catch (err) {
-    console("Error: Invalid or expired token, destroying all cookies");
+    console.log("Error: Invalid or expired token, destroying all cookies");
     Object.keys(req.cookies).forEach((cookie) => res.clearCookie(cookie));
     return res.status(401).json({
       success: false,
